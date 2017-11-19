c5db6df4bbdcc65e6c3d99ac1dc0de94a680b159
Stefan-Dator\Stefan <stefan85e@gmail.com>
2017-11-19 12:17:57 +0100

    Added a function enter_choice()

--- funktioner.h
+++ funktioner.h
@@ -6,8 +6,6 @@ double ohms_lag(double r, double i){
 
     return u;
 }
-
-
 //====================================//====================================
 double res_tot(double r1, double r2, double r3){
 
@@ -17,7 +15,6 @@ double res_tot(double r1, double r2, double r3){
 
     return rtot;
 }
-
 //====================================//====================================
 double eff_enk(double u, double i){
 
@@ -25,7 +22,6 @@ double eff_enk(double u, double i){
 
     return p;
 }
-
 //====================================//====================================
 double sken_eff(double u, double i){
 
@@ -33,7 +29,6 @@ double sken_eff(double u, double i){
 
     return s;
 }
-
 //====================================//====================================
 double aktiv_eff(double u, double i, double cos){
 
@@ -41,7 +36,6 @@ double aktiv_eff(double u, double i, double cos){
 
     return p;
 }
-
 //====================================//====================================
 double sken_3fas(double u, double i){
 
@@ -49,7 +43,23 @@ double sken_3fas(double u, double i){
 
     return s;
 }
+//============== Inpute value in I (Ampere) ================================
+void enter_choice()
+{
+    int val;
 
+    printf("V\x84lj vilka storheter du vill ber\x85kna:\n\n");
+    printf("V\x84lj 1 f\x94r: OHMS LAG\n");
+    printf("V\x84lj 2 f\x94r: Rtot\n");
+    printf("V\x84lj 3 f\x94r: EFFEKTLAGEN ENKEL\n");
+    printf("V\x84lj 4 f\x94r: SKENBAR EFFEKT ENFAS\n");
+    printf("V\x84lj 5 f\x94r: AKTIV EFFEKT/MEDELEFFEKT ENFAS\n");
+    printf("V\x84lj 6 f\x94r: SKENBAR EFFEKT 3-FAS\n");
+    printf("V\x84lj 7 f\x94r: AKTIV EFFEKT 3-FAS\n");
+    printf("V\x84lj 0 f\x94r: För ATT AVSLUTA\n");
+
+    scanf("%d", &val);
+}
 //====================================//====================================
 double aktiv_3fas(double u, double i, double cos){
 
@@ -57,8 +67,7 @@ double aktiv_3fas(double u, double i, double cos){
 
     return p;
 }
-
-//====================================//====================================
+//============== When put in wrong value ===================================
 void wrong_input()
 {
     while (1)
@@ -67,14 +76,12 @@ void wrong_input()
         continue;
     }
 }
-
-//====================================//====================================
+//============== Inpute value in U (Volt) ==================================
 void enter_volt()
 {
     printf("Skriv spännngen U i volt(V): \n ");
 }
-
-//====================================//====================================
+//============== Inpute value in I (Ampere) ================================
 void enter_ampere()
 {
     printf("gånger Strömmen I i ampere(A)\n\n");
--- main2.c
+++ main2.c
@@ -1,7 +1,4 @@
-/*
-Mer information om programets funktionalitet hittar ni i readme filen
-*/
-
+/* Mer information om programets funktionalitet hittar ni i readme filen */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -19,22 +16,10 @@ int main()
     {
         printf("\n");
         int val;
-
-        printf("V\x84lj vilka storheter du vill ber\x85kna:\n\n");
-        printf("V\x84lj 1 f\x94r: OHMS LAG\n");
-        printf("V\x84lj 2 f\x94r: Rtot\n");
-        printf("V\x84lj 3 f\x94r: EFFEKTLAGEN ENKEL\n");
-        printf("V\x84lj 4 f\x94r: SKENBAR EFFEKT ENFAS\n");
-        printf("V\x84lj 5 f\x94r: AKTIV EFFEKT/MEDELEFFEKT ENFAS\n");
-        printf("V\x84lj 6 f\x94r: SKENBAR EFFEKT 3-FAS\n");
-        printf("V\x84lj 7 f\x94r: AKTIV EFFEKT 3-FAS\n");
-        printf("V\x84lj 0 f\x94r: För ATT AVSLUTA\n");
-
-        scanf("%d", &val);
+        enter_choice();
 
         if(val == 1)
         {
-
             printf("Ohms lag spänningen(volt/V) betäckning U lika med Resistansen(Ohm) betäckning R \n");
             printf("gånger Strömmen(Ampere) med betäckningen I. Kort U=R*I. \n\n");
             double r, i;
@@ -44,16 +29,13 @@ int main()
             {
                 wrong_input();
             }
-
             printf("Skriv ström I < 440 Ampere: \n");
             scanf("%lf", &i);
             if(i > 440)
             {
                 wrong_input();
             }
-
             printf("%f V\n", ohms_lag(r, i));
-
         }
         else if(val == 2)
         {
@@ -82,7 +64,6 @@ int main()
         }
         else if(val == 3)
         {
-
             printf("Effektlagen enkel För likström är effekten P i Watt (W) lika med spänningen U i volt(V)\n");
             enter_ampere();
             double u, i;
@@ -98,7 +79,6 @@ int main()
         }
         else if(val == 4)
         {
-
             printf("Skenbar effekt enfas räknas med storheten VA(VoltAmpere) som är lika med spänningen U i volt(V)\n");
             enter_ampere();
             double u, i;
@@ -112,7 +92,6 @@ int main()
             }
             printf("%f VA\n", sken_eff(u, i));
         }
-
         else if(val == 5)
         {
             printf("Aktiv medelefdekt enfas är lika med effekt P i watt(W) lika med spänningen U i volt(V) gånger Strömmen I \n");
@@ -144,7 +123,6 @@ int main()
             {
                 wrong_input();
             }
-
             printf("Skriv ström I i ampere < 440: \n");
             scanf("%lf", &i);
             if(i > 440)
@@ -155,7 +133,6 @@ int main()
         }
         else if(val == 7)
         {
-
             printf("3-fas aktiv effekt är effekten P i Watt(W) lika med spänningen U i volt(V) gånger Strömmen I i ampere(A)\n");
             printf("gånger cos < 1 && cos > 0 gånger roten ur 3 SQRT(3).\n\n");
             double u, i, cos;
@@ -171,14 +148,12 @@ int main()
             {
                 wrong_input();
             }
-
             printf("Skriv in effektfaktorn cos > 0 && cos < 1: \n");
             scanf("%lf", &cos);
             if (cos < 0 && cos > 1)
             {
                 wrong_input();
             }
-
             printf("%f W\n", aktiv_3fas(u ,i, cos));
         }
         else if (val == 0)
@@ -190,6 +165,5 @@ int main()
             printf("Fel alternativ försök igen!: \n");
         }
     }
-
     return 0;
 }
